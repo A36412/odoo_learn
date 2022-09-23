f8510a3d44b135f7af7a1276c6c8a091b7c79a9d
Kien Do <dongockien92@gmail.com>
2022-09-23 16:58:16 +0700

    update

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -12,12 +12,16 @@
     'depends': ['base', 'mail', 'product'],
     'data': [
         'security/ir.model.access.csv',
+        'data/patient_tag_data.xml',
+        'data/sequence_data.xml',
+        'data/patient.tag.csv',
         'wizard/cancel_appointment_wizard.xml',
         'views/menu.xml',
         'views/patient_view.xml',
         'views/female_patient_view.xml',
         'views/appointment_view.xml',
         'views/patient_tag_view.xml',
+        'views/odoo_playground_view.xml'
     ],
     'demo': [],
     'auto_install': False,
