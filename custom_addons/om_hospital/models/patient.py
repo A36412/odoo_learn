83d79a0e2d9e591825924fc7393e1e007bbbd662
Kien Do <dongockien92@gmail.com>
2022-09-30 15:14:22 +0700

    inverse and res_config

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,6 +1,7 @@
 from datetime import date
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+from dateutil import relativedelta
 
 
 class HospitalPatient(models.Model):
@@ -12,7 +13,7 @@ class HospitalPatient(models.Model):
     name = fields.Char(string='Name', tracking=True)
     ref = fields.Char(string='Reference', tracking=True)
     date_of_birth = fields.Date(string='Date Of Birth')
-    age = fields.Integer(string="Age", compute='_compute_age', tracking=True)
+    age = fields.Integer(string="Age", compute="_compute_age", inverse="_inverse_compute_age", tracking=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", tracking=True)
     active = fields.Boolean(string="Active", default=True, tracking=True)
     appointment_id = fields.Many2one("hospital.appointment", string="Appointments")
@@ -38,7 +39,7 @@ class HospitalPatient(models.Model):
                 raise ValidationError(_("This Birth not acceptable"))
                 # Check ngày sinh nếu ngày sinh lớn hơn thời gian hiện tại thì sẽ xảy ra lỗi
 
-    @api.ondelete(at_uninstall=False) # Kiểm tra trước khi xóa  Patient xem có còn appointment nào thuộc Patient đó
+    @api.ondelete(at_uninstall=False)  # Kiểm tra trước khi xóa  Patient xem có còn appointment nào thuộc Patient đó
     def _check_appointments(self):
         for rec in self:
             if rec.appointment_ids:
@@ -52,16 +53,26 @@ class HospitalPatient(models.Model):
     def write(self, vals):
         if not self.ref and not vals.get('ref'):
             vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
-            return super(HospitalPatient, self).create(vals)
+        return super(HospitalPatient, self).write(vals)
 
     @api.depends('date_of_birth')
-    def _compute_age(self): #Đây là hàm tính toán ngày sinh
+    def _compute_age(self):  # Đây là hàm tính toán ngày sinh
         for rec in self:
             today = date.today()
             if rec.date_of_birth:
                 rec.age = today.year - rec.date_of_birth.year
             else:
-                rec.age = 0
+                rec.age = 1
+
+    @api.depends('age')
+    def _inverse_compute_age(self):
+        today = date.today()
+        for rec in self:
+            rec.date_of_birth = today - relativedelta.relativedelta(years=rec.age)
 
     def name_get(self):
         return [(record.id, "[%s] %s" % (record.ref, record.name)) for record in self]
+
+    def action_test(self):
+        print("Haiz")
+        return
